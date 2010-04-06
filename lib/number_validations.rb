1de50a761cc009edc22c53bc3f8409e795061206
Yamada Masaki <masaki@hisme.net>
2010-04-06 14:06:53 +0900

    add method for validate positive number

--- lib/number_validations.rb
+++ lib/number_validations.rb
@@ -2,6 +2,14 @@
 module ActiveRecord
   module Validations
     module ClassMethods
+      def validates_positive_number_of(*attr_names)
+        configuration = {:on => :save}
+        configuration.update(attr_names.extract_options!)
+        validates_each(attr_names, configuration) do |record, attr_name, value|
+          record.errors.add(attr_name, 'must be positive number') if value <= 0
+        end
+      end
+      
       def validates_incremental_of(*attr_names)
         configuration = { :on => :update }
         configuration.update(attr_names.extract_options!)
